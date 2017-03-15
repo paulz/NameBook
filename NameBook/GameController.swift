002d7060f1bc79c1a3f4f5bfab909d5a0f820cf4
Paul Zabelin <pzabelin+git@gmail.com>
2017-03-15 03:18:33 -0700

    show failures again

--- NameBook/GameController.swift
+++ NameBook/GameController.swift
@@ -1,40 +1,47 @@
 import Foundation
 
 struct GameController {
-    let gameContacts: [String]
-    var queueToPlay: [String]
+    let gameContacts: Set<String>
+    var queueToPlay: [String] = []
     var choices: [String] = []
     var correct: Int = -1
 
     init(contacts: [String]) {
-        gameContacts = contacts
-        queueToPlay = gameContacts
-        queueToPlay.shuffle()
+        gameContacts = Set(contacts)
     }
 
     func correctChoice() -> String {
         return choices[correct]
     }
 
-    func isCorrect(selection:Int) -> Bool {
-        return correct == selection
+    mutating func isCorrect(selection:Int) -> Bool {
+        let won = correct == selection
+        if !won {
+            queueToPlay.insert(choices[selection], at: 0)
+            queueToPlay.insert(choices[correct], at: 0)
+            queueToPlay.shuffle()
+        }
+        return won
     }
 
     mutating func nextRound()  {
+        if queueToPlay.count < 6 {
+            queueToPlay = Array(gameContacts)
+            queueToPlay.shuffle()
+        }
         choices = randomContacts(count: 6)
         correct = Int(arc4random_uniform(UInt32(choices.count)))
     }
 
-    func randomContacts(count: Int) -> [String] {
-        var selection = gameContacts
+    mutating func randomContacts(count: Int) -> [String] {
         var selected: [String] = []
         for _ in 1...count {
-            if selection.count == 0 {
+            if queueToPlay.count == 0 {
                 break
             }
-            let random = Int(arc4random_uniform(UInt32(selection.count)))
-            let contact = selection[random]
-            selection.remove(at: random)
+            let random = Int(arc4random_uniform(UInt32(queueToPlay.count)))
+            let contact = queueToPlay[random]
+            queueToPlay.remove(at: random)
             selected.append(contact)
         }
         return selected
