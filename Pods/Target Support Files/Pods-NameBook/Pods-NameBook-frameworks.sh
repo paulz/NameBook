60268a10baf6ad64c4dd30af695efb76720d63b3
Paul Zabelin <pzabelin+git@gmail.com>
2017-03-17 22:15:55 -0700

    add SwinjectStoryboard pod

--- Pods/Target Support Files/Pods-NameBook/Pods-NameBook-frameworks.sh
+++ Pods/Target Support Files/Pods-NameBook/Pods-NameBook-frameworks.sh
@@ -87,6 +87,15 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Swinject/Swinject.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwinjectStoryboard/SwinjectStoryboard.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Swinject/Swinject.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwinjectStoryboard/SwinjectStoryboard.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
